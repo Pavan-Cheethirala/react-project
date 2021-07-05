15a20a6e0ebc3ae094be457de30597f03cd11897
ABDULLA4G3 <abdulla4g3@sasi.ac.in>
2021-07-05 06:15:29 -0700

    Updates

---
+++ precache-manifest.b853d089738eaa3cbe8e4e67c26ea6a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf4aef5d6ce9e1d3d9b0",
+    "url": "/react-project/static/css/main.d636b235.chunk.css"
+  },
+  {
+    "revision": "bf4aef5d6ce9e1d3d9b0",
+    "url": "/react-project/static/js/main.bf4aef5d.chunk.js"
+  },
+  {
+    "revision": "f608697f507c53c2a042",
+    "url": "/react-project/static/js/1.f608697f.chunk.js"
+  },
+  {
+    "revision": "0a54ebb76cbb06400fd1",
+    "url": "/react-project/static/js/runtime~main.0a54ebb7.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/react-project/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/react-project/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/react-project/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/react-project/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/react-project/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "d5182044463c5b9992ad438eace90534",
+    "url": "/react-project/index.html"
+  }
+];
